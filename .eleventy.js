f1b836b9c39248a9d132473aea924834b64a30de
madjalapeno <github@jalapeno.co.uk>
2025-03-17 16:30:08 -0400

    swapped from jekyll to 11ty

---
+++ .eleventy.js
@@ -0,0 +1,58 @@
+const now = String(Date.now())
+
+// Image transform plugin
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+// add id to headings
+import markdownIt from "markdown-it";
+import markdownItAnchor from "markdown-it-anchor";
+
+import pluginFilters from "./filters.mjs";
+
+//import dotenv from 'dotenv';
+//dotenv.config();
+
+export default function (eleventyConfig) {
+  eleventyConfig.setInputDirectory("src");
+  eleventyConfig.setIncludesDirectory("_includes");
+  eleventyConfig.setLayoutsDirectory("_layouts");
+
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts");
+  eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
+  
+  eleventyConfig.addWatchTarget("src");
+
+  // read data from .env file to determine dev or prod
+  eleventyConfig.addGlobalData("env", process.env.ELEVENTY_ENV);
+
+  // Plug Ins
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin,{
+    formats: ["avif", "webp", "jpeg"],
+  });
+  
+  let markdownLibrary = markdownIt({
+    html: true,
+  });
+  
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
+  // Shortcodes
+  eleventyConfig.addShortcode('version', function () {
+    return now
+  });
+  eleventyConfig.addShortcode('year', function () {
+    return new Date().getFullYear()
+  });
+
+  eleventyConfig.addFilter("ucfirst", function(value) {
+    return String(value).charAt(0).toUpperCase() + String(value).slice(1);
+  });
+
+	eleventyConfig.addPlugin(pluginFilters);
+};
+
+export const config = {
+  htmlTemplateEngine: "njk",
+  markdownTemplateEngine: "njk"
+};
---
+++ src/posts/posts.11tydata.mjs
@@ -0,0 +1,6 @@
+export default {
+	tags: [
+		"posts"
+	],
+	"layout": "/_layouts/post.njk",
+};
